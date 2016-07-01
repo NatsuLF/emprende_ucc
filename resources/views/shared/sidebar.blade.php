3fbc4ba103d15ff7c1372353680a45d38414b59c
Leonel Barrera <leodota7@gmail.com>
2016-07-01 10:46:11 -0600

    Implemented getting controllerName from views for setting class to active in sidebar

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,5 +1,11 @@
 <ul class="nav nav-pills nav-stacked">
-  <li role="presentation"><a href="#">Posts</a></li>
-  <li role="presentation" class="active"><a href="#">Categorias</a></li>
-  <li role="presentation"><a href="#">Productos</a></li>
+    <li role="presentation" class="{{ $controller == 'PostController' ? 'active' : 'no-active' }}">
+        <a href="{{ url('/posts') }}">Posts</a>
+    </li>
+    <li role="presentation" class="{{ $controller == 'TagController' ? 'active' : 'no-active' }}">
+        <a href="{{ url('/tags') }}">Categorias</a>
+    </li>
+    <li role="presentation" class="{{ $controller == 'ItemController' ? 'active' : 'no-active' }}">
+        <a href="#">Productos</a>
+    </li>
 </ul>
