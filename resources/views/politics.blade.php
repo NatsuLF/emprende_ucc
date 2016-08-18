6ab7fee8f92f275d59615112786adc8fcd4a3d18
Leonel Barrera <leodota7@gmail.com>
2016-08-18 10:55:27 -0600

    Finish new recomends for JD

--- resources/views/politics.blade.php
+++ resources/views/politics.blade.php
@@ -4,15 +4,85 @@
 
 @section('content')
 
-    <div class="col-md-12">
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-    </div>
+    <div class="col-md-1"></div>
+        <div class="col-md-10 text-justify">
+            <div class="page-header">
+              <h1>Politicas del PEIDE</h1>
+            </div>
+            <p>
+                Hay, al menos, tres cuestiones clave relacionadas con el contexto económico de Nicaragua
+                que afectará a las políticas de PEIDE: la relevancia de las MIPYMES a la economía; la
+                importancia dada a la innovación en el país; y los desafíos de la informalidad. Además, existe
+                el contexto interno de la propia compañía para ser considerado.
+            </p>
+            <p>
+                Según el estudio de 2015 realizado por Cosep, “Encuesta de Empresas Sostenibles”, el 80%
+                de las empresas nicaragüenses son micro y pequeñas empresas (MiPYMES), que tiene
+                menos de 20 empleados. Dada la debilidad del mercado de trabajo formal en Nicaragua, el
+                espíritu empresarial es un medio clave para los nicaragüenses es promover y desarrollar sus
+                carreras al mismo tiempo ganar dinero para mantener a sus familias. Por desgracia, las
+                habilidades necesarias para hacer crecer el negocio a menudo están ausentes. El mismo
+                estudio demostró que Cosep en la muestra de 2.539 empresas, el 51% de las empresas son
+                informales (no registrado) y el 37% no tienen forma de contabilidad. La necesidad de
+                asistencia técnica y apoyo es grande.
+            </p>
+            <p>
+                Las habilidades empresariales son igualmente valiosos para aquellos que no están
+                interesados en ser un empresario. Pueden ser utilizados para apoyar a las empresas de la
+                familia o el trabajo como consultor en otras empresas. Por otra parte, las grandes empresas
+                (7%) de las empresas nicaragüenses necesitan las habilidades de los empresarios internos
+                (intrapreneurship) con el fin de innovar y seguir siendo competitivos.
+            </p>
+            <p>
+                Este fuerte énfasis en la innovación es relevante debido a que es un área que parece tener
+                un apoyo insuficiente. Un ranking 2015 de Nicaragua en términos de innovación muestra
+                una posición de 130 de los 141 países evaluados por la Organización Mundial de la
+                Propiedad Intelectual (OMPI). El ranking 2013 era mejor en 115 de 142, pero la necesidad
+                de un esfuerzo más concertado en la innovación es claramente necesario. En 2015, menos
+                del 0,1% del PIB se invirtió en la innovación, que es comparable al de otros países de
+                América Central como El Salvador, Guatemala y Honduras, pero significativamente inferior
+                a la media del 0,7% para los países de América Latina en general. La innovación no es un fin
+                en sí mismo sino un medio para diferenciarse de la competencia, y la más importante para
+                MiPYMEs que puede poner en práctica los cambios más rápido que las grandes empresas
+                lo hacen.
+            </p>
+            <p>
+                El tema de la informalidad es también uno que debe ser evaluado para determinar la
+                manera de promover emprendimiento, especialmente en lo que parece haber consenso en
+                que el problema está creciendo. De acuerdo con el estudio de 2015 Cosep, La Encuesta de
+                Empresas Sostenibles, el 55-60% de la población activa trabajaba en el sector informal en
+                2006, mientras que ha aumentado un 80% en 2015. Por otra parte, el 75% de las empresas
+                informales parecen funcionar con algunos problemas, por lo tanto, con poca motivación
+                para formalizar. Aunque UCC-León trabaja con empresas del sector formal, que tendrá que
+                considerar la forma de promover y facilitar la formalización de empresas que buscan
+                orientación.
+            </p>
+            <p>
+                Este contexto nacional (y regional) es de vital importancia en el diseño e implementación
+                de un programa para PEIDE pero igualmente importante es el contexto interno de las
+                MIPYMES que la UCC va a apoyar. Las pequeñas empresas varían mucho - por tipo de
+                negocio, la geografía, el tamaño, los riesgos que enfrentan, y ambiciones de crecimiento.
+                Dos elementos clave para PEIDE para determinar qué empresas a incluir en su programa es
+                la edad de la empresa y el sector de actividad.
+            </p>
+            <p>
+                El ciclo de vida de una nueva empresa es particularmente difícil
+                en los primeros cinco años. Por otra parte, las necesidades durante el período pre-
+                operativa, así como durante los primeros meses son diferentes de cuando la Compañía
+                comienza a desarrollar (seis meses - 3,5 años) y evolucionar (más de 3,5 años consecutivos).
+                Como resultado, la posible participación de PEIDE variará en función de la fase de desarrollo
+                de la empresa.
+            </p>
+            <p>
+                Del mismo modo, el sector de actividad en el que opera una empresa (o operará) influirá
+                en sus necesidades, así como el valor añadido potencial de PEIDE para asistir como asesor.
+                El contexto, tanto externos como internos, por lo tanto, va a ser relevante como PEIDE
+                determina la mejor manera de cumplir con su Misión y estrategia.
+            </p>
 
+            <a href="https://drive.google.com/file/d/0B0qJycjoKzWpaHE2SEJ0X3V0RGc/view?usp=sharing" class="btn btn-default" title="Politica emprendimiento" target="_blank">
+                <i class="fa fa-cloud-download" aria-hidden="true"></i> Politica de emprendedurismo 2016
+            </a>
+        </div>
+    <div class="col-md-1"></div>
 @endsection
--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -25,7 +25,7 @@
 	<div class="col-md-6">
 		<div class="panel panel-success">
 			<div class="panel-heading">
-				<h3 class="panel-title">Modulo Formador de Formadores</h3>
+				<h3 class="panel-title">Modulo formador de formadores</h3>
 			</div>
 			<div class="panel-body">
 				<p>
@@ -79,6 +79,23 @@
 		<div class="panel panel-success">
 			<div class="panel-heading">
 				<h3 class="panel-title">Mentoria a empresas</h3>
+			</div>
+			<div class="panel-body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">Aceleradora de negocios</h3>
 			</div>
 			<div class="panel-body">
 				<p>
