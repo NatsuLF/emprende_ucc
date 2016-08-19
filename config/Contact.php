c6cf0d1595d7a07e2722a8ddc72060f5990dec4d
Leonel Barrera <leodota7@gmail.com>
2016-08-19 16:46:18 -0600

    Change contact info

--- config/Contact.php
+++ config/Contact.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Config;
 return [
     'address' => 'Costado Oeste Campus Medico - UNAN-Leon',
     'email' => 'juan.mairena@ucc.edu.ni',
-    'telephones' => ['2311-0811', '8810-1599']
+    'telephones' => ['2311-0811/12', '7516-9997']
 ];
