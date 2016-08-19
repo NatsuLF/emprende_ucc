6999ddeb10bc0f77fecf970487dbceac295438b6
Leonel Barrera <leodota7@gmail.com>
2016-08-19 17:15:33 -0600

    Last commit

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -111,16 +111,16 @@
 	<div class="col-md-6">
 		<div class="panel panel-success">
 			<div class="panel-heading">
-				<h3 class="panel-title">Aceleradora de negocios</h3>
+				<h3 class="panel-title">Incubadora y Aceleradora de Negocios</h3>
 			</div>
 			<div class="panel-body">
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				PEIDE proporcionará apoyo anualmente a 10-12 proyectos dirigidos
+				por estudiantes.
+				La asistencia será por un período de 12 meses de acuerdo con el diagrama,
+				con seis meses para la fase de preparación y seis meses una vez que han
+				comenzado elaboraciones. Los proyectos de los estudiantes no estarán
+				limitados ni por sector de actividad ni por categoría.
 				</p>
 	            <a href="https://drive.google.com/file/d/0B0qJycjoKzWpaHE2SEJ0X3V0RGc/view?usp=sharing" class="btn btn-default btn-block" title="Politica emprendimiento" target="_blank">
 	                <i class="fa fa-cloud-download" aria-hidden="true"></i> Descargar pdf
